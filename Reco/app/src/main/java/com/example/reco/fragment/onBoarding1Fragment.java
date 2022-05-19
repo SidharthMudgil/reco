69e2f89f50124545a2747c6f6c672c83d8f4f1bf
SidharthMudgil <68889544+SidharthMudgil@users.noreply.github.com>
2022-05-19 16:22:23 +0530

    onBoarding fragments added

---
+++ Reco/app/src/main/java/com/example/reco/fragment/onBoarding1Fragment.java
@@ -0,0 +1,28 @@
+package com.example.reco.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.reco.R;
+
+public class onBoarding1Fragment extends Fragment {
+
+    public onBoarding1Fragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_on_boarding1, container, false);
+    }
+}
