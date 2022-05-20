b9d163500cc3ca78c5d1d3ae869f6264925b5707
SidharthMudgil <68889544+SidharthMudgil@users.noreply.github.com>
2022-05-20 16:04:51 +0530

    onBoardingFragments replaced with single slide_layout

---
+++ Reco/app/src/main/java/com/example/reco/OnBoardingActivity.java
@@ -0,0 +1,14 @@
+package com.example.reco;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OnBoardingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_boarding);
+    }
+}
