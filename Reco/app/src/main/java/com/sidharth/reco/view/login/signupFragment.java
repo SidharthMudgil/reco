0a819115655cdc1f3c7192c7322cb4dea324407a
SidharthMudgil <68889544+SidharthMudgil@users.noreply.github.com>
2022-05-22 10:12:16 +0530

    login and signUp fragments added

---
+++ Reco/app/src/main/java/com/sidharth/reco/view/login/signupFragment.java
@@ -0,0 +1,27 @@
+package com.sidharth.reco.view.login;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sidharth.reco.R;
+
+public class signupFragment extends Fragment {
+    public signupFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_signup, container, false);
+    }
+}
