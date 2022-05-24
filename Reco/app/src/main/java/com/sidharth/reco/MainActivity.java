033b7e2211a4ce2cfe99c523c48e7fb16c7bb593
SidharthMudgil <68889544+SidharthMudgil@users.noreply.github.com>
2022-05-24 10:28:36 +0530

    UI dimensions modified

--- Reco/app/src/main/java/com/sidharth/reco/MainActivity.java
+++ Reco/app/src/main/java/com/sidharth/reco/MainActivity.java
@@ -1,18 +1,46 @@
 package com.sidharth.reco;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.sidharth.reco.view.login.LoginActivity;
+import com.sidharth.reco.view.onboarding.OnBoardingActivity;
+
 public class MainActivity extends AppCompatActivity {
+
+    static private final int STATE_ON_BOARDING = 0;
+    static private final int STATE_LOGIN_SIGNUP = 1;
+    static private final int STATE_SPLASH_APP = 2;
+
+    static private final String STATE = "sidharth.reco.state";
+
+    private Intent intent = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, LoginActivity.class);
+        SharedPreferences sharedPreferences = getSharedPreferences(getPackageName(), MODE_PRIVATE);
+        int state = sharedPreferences.getInt(STATE, 0);
+
+        switch (state) {
+            case STATE_ON_BOARDING:
+                intent = new Intent(this, OnBoardingActivity.class);
+                startActivity(intent);
+                break;
+            case STATE_LOGIN_SIGNUP:
+                intent = new Intent(this, LoginActivity.class);
+                break;
+            case STATE_SPLASH_APP:
+//                intent = new Intent(this, .class);
+                break;
+            default:
+                finish();
+        }
         startActivity(intent);
     }
 }
--- Reco/app/src/main/java/com/sidharth/reco/view/login/LoginActivity.java
+++ Reco/app/src/main/java/com/sidharth/reco/view/login/LoginActivity.java
@@ -1,14 +1,10 @@
-package com.sidharth.reco;
+package com.sidharth.reco.view.login;
 
 import android.os.Bundle;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager2.widget.ViewPager2;
 
-import com.google.android.material.tabs.TabLayout;
-import com.google.android.material.tabs.TabLayoutMediator;
-import com.sidharth.reco.controller.TabPagerAdapter;
+import com.sidharth.reco.R;
 
 public class LoginActivity extends AppCompatActivity {
 
