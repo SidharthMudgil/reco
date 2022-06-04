3e9dc618132203ca3c63ab4f3b898c56295ca34a
SidharthMudgil <68889544+SidharthMudgil@users.noreply.github.com>
2022-06-04 23:26:57 +0530

    1. code optimized: Log statements removed 2. ap ready for build version

--- Reco/app/src/main/java/com/sidharth/reco/login/view/SignUpFragment.java
+++ Reco/app/src/main/java/com/sidharth/reco/login/view/SignUpFragment.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
